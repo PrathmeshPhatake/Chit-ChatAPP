fc69d5a1b32b011d9a8cfb008565352da5e2ce6a
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2025-01-15 15:12:58 +0530

    connect login page to baceknd

--- Firebase-reactchat/src/components/login/Login.jsx
+++ Firebase-reactchat/src/components/login/Login.jsx
@@ -2,100 +2,121 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import avatar_png from "../../../public/avatar.png";
 import "../login/login.css";
-import axios from 'axios';
+import axios from "axios";
 import { uploadfile } from "../../helper/uploadfile.jsx";
 
-
-const API= import.meta.env.VITE_BACKEND_URL;
-const URL = `http://localhost:4001/user/register`;
+const API = import.meta.env.VITE_BACKEND_URL;
 
 const Login = () => {
   const [avtar, setAvtar] = useState({
     file: null,
     url: "",
   });
-  // for registration 
-  const [registrationData,setRegistrationData]=useState({
-    Username:"",
-    email:" ",
-    password:"",
-    profile_pic: null
-  })
-  // for login 
-  const [loginData,setLoginData]=useState({
-    email:"",
-    password:""
-  })
+  // for registration
+  const [registrationData, setRegistrationData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_pic: null,
+  });
+  // for login
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleRegistrationChange =async(e)=>{
-    const {name,value}=e.target
-    setRegistrationData((prev)=>{
+  const handleRegistrationChange = async (e) => {
+    const { name, value } = e.target;
+    setRegistrationData((prev) => {
       return {
         ...prev,
-        [name]:value
-      }
-    })
-  }
-  const handleLoginChange =(e)=>{
-    const {name,value}=e.target
-    setLoginData((prev)=>{
+        [name]: value,
+      };
+    });
+  };
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prev) => {
       return {
         ...prev,
-        [name]:value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
 
   // add avtar their
-  const handleAvtar =async (e) => {
-    const file= e.target.files[0]
+  const handleAvtar = async (e) => {
+    const file = e.target.files[0];
     if (file) {
-      const uplaodPhoto=await uploadfile(file);
-      console.log("uploadPhoto:",uplaodPhoto);
+      const uplaodPhoto = await uploadfile(file);
+      console.log("uploadPhoto:", uplaodPhoto);
       setAvtar({
-        files: file,
-        url: URL.createObjectURL(file),
+        file: file,
+        url: window.URL.createObjectURL(file),
       });
-      setRegistrationData((prev)=>{
+      setRegistrationData((prev) => {
         return {
           ...prev,
-          profile_pic:file.name
-        }
-      })
+          profile_pic: file.name,
+        };
+      });
     }
   };
-  
-  console.log("avtar:",avtar);
 
+  // console.log("avtar:",avtar);
 
-  const handleRegistration=async(e)=>{
+  const handleRegistration = async (e) => {
     e.preventDefault();
-    e.stopPropagation()
-      // stor in backend
-  //  const URL=`${API}/user/register`;
-   try {
-    const response=await axios.post(URL,registrationData);
-    console.log("response",response);
-    toast.success("sign_IN succesfuly!");
-   } catch (error) {
-     console.log("error",error);
-   }
-    console.log("Data:",registrationData);
-  }
+    e.stopPropagation();
+    // stor in backend
+    //  const URL=`${API}/user/register`;
+    try {
+      const URL = `${API}/user/register`;
+      const response = await axios.post(URL, registrationData);
+      console.log("response", response);
+
+      toast.success(response.data.success);
+      if(response.data.success)
+      {
+        setRegistrationData({
+          name:"",
+          email:"",
+          password:"",
+          profile_pic:"",
+        });
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      console.log("error", error);
+    }
+    console.log("Data:", registrationData);
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
     e.stopPropagation();
     toast.success("Login Successful!");
-    console.log("DATA:",loginData);
+    console.log("DATA:", loginData);
   };
   return (
     <div className="login">
       <div className="item">
         <h2> Welcome back,</h2>
         <form onSubmit={handleLogin}>
-          <input type="text" placeholder="Email" name="email" value={loginData.email} onChange={handleLoginChange} />
-          <input type="password" placeholder="Password" name="password" value={loginData.password} onChange={handleLoginChange}/>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={loginData.email}
+            onChange={handleLoginChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={loginData.password}
+            onChange={handleLoginChange}
+          />
           <button>Log_IN</button>
         </form>
       </div>
@@ -111,12 +132,29 @@ const Login = () => {
             type="file"
             id="file"
             style={{ display: "none" }}
-           
             onChange={handleAvtar}
           />
-          <input type="text" placeholder="Username" name="Username" value={registrationData.Username} onChange={handleRegistrationChange} />
-          <input type="text" placeholder="Email" name="email" value={registrationData.email} onChange={handleRegistrationChange} />
-          <input type="password" placeholder="Password" name="password" value={registrationData.password}  onChange={handleRegistrationChange} />
+          <input
+            type="text"
+            placeholder="Username"
+            name="name"
+            value={registrationData.name}
+            onChange={handleRegistrationChange}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={registrationData.email}
+            onChange={handleRegistrationChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={registrationData.password}
+            onChange={handleRegistrationChange}
+          />
           <button>Sign_IN</button>
         </form>
       </div>
