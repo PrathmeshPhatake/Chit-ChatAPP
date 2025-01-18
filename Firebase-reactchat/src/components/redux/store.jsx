6c427a5f9695c83e7905c2f290ef6c5c3d7db0e6
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2025-01-19 03:06:55 +0530

    add reduxtoolkit

--- Firebase-reactchat/src/components/redux/store.jsx
+++ Firebase-reactchat/src/components/redux/store.jsx
@@ -2,7 +2,7 @@ import {  configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice.jsx'
 
 export  const store = configureStore({
-    reducer: {
+    reducer: {  
         user:userReducer
     }
   })
